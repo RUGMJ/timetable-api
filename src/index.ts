c84fc7be33057273a0c725dd1ed48cd9c6332486
rugmj <rugmj@rugmj.dev>
2023-01-23 18:52:04 +0000

    Changed the cached value to just the week

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,15 @@ app.use(express.json());
 
 let { CACHE_TIME, PORT } = process.env;
 
-let lastRetrivedLessons = Date.now();
-let lessons: { subject: string; teacher: string; class: string }[];
+let lastRetrivedWeek = Date.now();
+let week: number;
 
 app.get('/', async (req, res) => {
 	res.send(await getLessons());
 });
 
 app.get('/debug/refreshCache', async (req, res) => {
-	lessons = undefined;
+	week = undefined;
 	res.send(await getLessons());
 });
 
@@ -33,16 +33,11 @@ app.get('/debug/getInfo', async (req, res) => {
 app.listen(parseInt(PORT));
 
 async function getLessons() {
-	const timeSinceLastCheck = Date.now() - lastRetrivedLessons;
-	if (timeSinceLastCheck < parseInt(CACHE_TIME) && lessons) return lessons;
-
 	const week = await getWeek();
 
 	const day = getDay();
 
-	lastRetrivedLessons = Date.now();
-	lessons = timetable[week][day];
-	return lessons;
+	return timetable[week][day];
 }
 
 function getDay() {
@@ -50,11 +45,14 @@ function getDay() {
 }
 
 async function getWeek() {
+	const timeSinceLastCheck = Date.now() - lastRetrivedWeek;
+	if (timeSinceLastCheck < parseInt(CACHE_TIME) && week) return week;
+
 	const webdata = await fetch('https://www.stjuliansschool.co.uk/');
 
 	const dom = new JSDOM(await webdata.text());
 
-	const week =
+	week =
 		dom.window.document.querySelector('.week>.active').textContent === 'Week 1'
 			? 0
 			: 1;
